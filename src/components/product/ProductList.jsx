d82c59ad5d762502266e2539ab8e699c73ba4303
Vidur Sakhiya <vidursakhiya7294@gmail.com>
2025-10-15 19:01:19 +0530

    initial commit

---
+++ src/components/product/ProductList.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const ProductList = () =>{
+    const product = useState([]);
+    // const fetchPro
+    // useEffect(()=>{
+    //     try {
+    //         const resp = await axios.post();
+    //     } catch (err) {
+
+    //     }
+    // },[]);
+
+    return (
+        <></>
+    );
+}
